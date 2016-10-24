f8db71d9c8ca3c70f43f111acd7f38fba8194986
dzucconi <mail@damonzucconi.com>
2016-10-24 14:13:18 +0200

    Reserve some more slugs

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -1,6 +1,6 @@
 FriendlyId.defaults do |config|
   config.use :reserved
-  config.reserved_words = %w(new edit session login logout users admin stylesheets assets javascripts images)
+  config.reserved_words = %w(new edit session login logout users admin stylesheets assets javascripts images random source)
   config.use :finders
   config.use :slugged
 end
