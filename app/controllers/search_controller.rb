8e237e8de6dc085fa891a0d326853f4a996de851
dzucconi <mail@damonzucconi.com>
2016-10-24 21:52:12 +0200

    Authenticate the search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
+  before_filter :authenticate!
+
   def index
     @query = params[:q]
     @pages = Page.basic_search(@query).page(params[:page]).per(params[:per]) if @query.present?
