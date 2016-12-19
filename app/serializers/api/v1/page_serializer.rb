f169b12cb316584f886dec206f9fff7ba26d3781
dzucconi <mail@damonzucconi.com>
2016-12-19 23:01:47 +0100

    Returns the content as the "html" as well in certain modes

--- app/serializers/api/v1/page_serializer.rb
+++ app/serializers/api/v1/page_serializer.rb
@@ -1,12 +1,34 @@
 module Api
   module V1
     class PageSerializer < BaseSerializer
-      attributes :id, :slug, :mode, :title, :delta, :content, :html, :created_at, :updated_at, :_links
+      attributes :id,
+        :slug,
+        :mode,
+        :title,
+        :delta,
+        :content,
+        :html,
+        :created_at,
+        :updated_at,
+        :_links
+
+      def html
+        case object.mode
+        when 'plain', 'html'
+          object.content
+        else
+          object.html
+        end
+      end
 
       def _links
         {
-          self: { href: api_namespace_page_url(object.namespace, self.slug) },
-          namespace: { href: api_namespace_url(object.namespace) }
+          self: {
+            href: api_namespace_page_url(object.namespace, self.slug)
+          },
+          namespace: {
+            href: api_namespace_url(object.namespace)
+          }
         }
       end
     end
